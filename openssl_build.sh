0ac399a72b83388a314f371576ee24f54f07a21b
dirk <info@universalappfactory.de>
2023-11-13 21:51:59 +0100

    ci: use clang

--- openssl_build.sh
+++ openssl_build.sh
@@ -27,7 +27,7 @@ function build_library {
 if [ "$ANDROID_TARGET_ABI" == "armeabi-v7a" ]
 then
     export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
-    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
+    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-clang/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-clang/prebuilt/linux-x86_64/bin:$PATH
     cd ${OPENSSL_TMP_FOLDER}
     ./Configure android-arm -D__ANDROID_API__=${ANDROID_TARGET_API} no-tests --prefix=${OUTPUT_PATH}
     build_library
@@ -35,7 +35,7 @@ then
 elif [ "$ANDROID_TARGET_ABI" == "arm64-v8a" ]
 then
     export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
-    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
+    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-clang/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-clang/prebuilt/linux-x86_64/bin:$PATH
     cd ${OPENSSL_TMP_FOLDER}
     ./Configure android-arm64 -D__ANDROID_API__=${ANDROID_TARGET_API} no-tests --prefix=${OUTPUT_PATH}
     build_library
@@ -43,7 +43,7 @@ then
 elif [ "$ANDROID_TARGET_ABI" == "x86" ]
 then
     export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
-    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
+    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-clang/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-clang/prebuilt/linux-x86_64/bin:$PATH
     cd ${OPENSSL_TMP_FOLDER}
     ./Configure android-x86 -D__ANDROID_API__=${ANDROID_TARGET_API} no-tests --prefix=${OUTPUT_PATH}
     build_library
@@ -51,7 +51,7 @@ then
 elif [ "$ANDROID_TARGET_ABI" == "x86_64" ]
 then
     export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
-    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
+    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-clang/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-clang/prebuilt/linux-x86_64/bin:$PATH
     cd ${OPENSSL_TMP_FOLDER}
     ./Configure android-x86_64 -D__ANDROID_API__=${ANDROID_TARGET_API} no-tests --prefix=${OUTPUT_PATH}
     build_library
